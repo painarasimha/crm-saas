bbb42f89a3780f86fd874a9105f573e045d989c9
painarasimha <pai.narasimha04@gmail.com>
2025-07-12 10:11:57 +0530

    code the initial server logic

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+const authRoutes = require('./src/routes/auth');
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
